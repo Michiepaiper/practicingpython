d8b8cbbe1e9596e8ad9af6e050c8a8fe34933195
Michelle Piper <michellepiper31@gmail.com>
2022-09-18 17:57:17 +0100

    maths.py

---
+++ maths.py
@@ -0,0 +1,6 @@
+info = 'a + b' is ab. For example 9+1 will be 91 if float is not added to the command. for example:
+
+    print('welcome to super simple calculator')
+    a = Float(input('enter first number : '))
+    b = Float(input('enter second number : '))
+    print('your answer is', a + b)
