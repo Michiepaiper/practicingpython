ddf48dc19cfad857f4d0510f6adb46f1b627fa5f
Michelle Piper <michellepiper31@gmail.com>
2022-10-04 19:24:02 +0100

    looping

---
+++ loop.py
@@ -0,0 +1,7 @@
+# By Michelle Piper
+# Oct 4th,2022 - 07:15 pm
+
+# Write a script that test if 2 divided by 2 gives 0
+
+i = 2
+print((i % 2) == 0)
---
+++ looptest.py
@@ -0,0 +1,15 @@
+# By Michelle Piper
+# Oct 4th, 2022 - 07:13pm
+
+# write a script that prints odd numbers
+
+i = 20
+# use for loop through the list from 1-21
+for i in range (1,21):
+
+    #use modulus to check that the result is NOT EQUAL to 0
+    if ((i % 2) != 0):
+
+        #print the odds
+        print( "i =  ",i)
+
